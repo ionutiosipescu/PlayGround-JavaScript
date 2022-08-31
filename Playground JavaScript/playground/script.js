31d108b9b6292421fd08f7b7b0d524fc0a9eafd2
ionutiosipescu <ionutiosipescu@yahoo.com>
2022-08-31 17:29:06 +0300

    update

--- Playground JavaScript/playground/script.js
+++ Playground JavaScript/playground/script.js
@@ -18,3 +18,23 @@
 
 // console.log(longestWords(`I woke up early today`));
 // console.log(longestWords(`I went straight to the beach`));
+
+
+const people = [
+    { name: "Rohan", age: 18 },
+    { name: "Mohan", age: 20 },
+    { name: "Shawn", age: 18 },
+    { name: "Michael", age: 18 },
+    { name: "David", age: 20 },
+ ];
+
+ let groupBy = (array, key) => {
+    return array.reduce((result, obj) => {
+       (result[obj[key]] = result[obj[key]] || []).push(obj);
+       return result;
+    }, {});
+ };
+
+ let a = groupBy(people, "age");
+ 
+ console.log(a)
